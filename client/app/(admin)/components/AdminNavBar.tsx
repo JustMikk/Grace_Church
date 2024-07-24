322d681ffb49aa0f305a639334c847d39490ba66
Mikiyas Hailegebriel <mikiyasbutu@gmail.com>
2024-07-23 23:02:35 -0800

    Created admin routes and a navbar layout

---
+++ client/app/(admin)/components/AdminNavBar.tsx
@@ -0,0 +1,11 @@
+import Logo from "@/components/navbar/Logo";
+
+const AdminNavbar = () => {
+  return (
+    <nav className="w-full h-20 px-10 py-2 bg-white flex items-center justify-end shadow-2xl ">
+      <Logo />
+    </nav>
+  );
+};
+
+export default AdminNavbar;
